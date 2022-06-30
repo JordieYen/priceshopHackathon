258bfd492390f3917ba3829e4db2d74e514e9066
Jordan King yen hii <jking-ye@u82z06s01.42kl.edu.my>
2022-06-30 21:54:02 +0800

    added fuzzy proto

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
-	Python3 printCSV.py csv/LazadaLinks.csv
+	Python3 printCSV.py csv/LazadaLinks.csv > csv/Output.csv
+merchant:
+	Python3 GetMerchantLinks.py csv/Merchantlinks.csv > csv/Links.csv
 test:
 	Python3 printCSV.py csv/test.csv
--- fuzzy.py
+++ fuzzy.py
@@ -7,48 +7,69 @@ import json
 import re
 import requests
 
+def remove_consec_dash(s):
+    new_s = ""
+    prev = ""
+    for c in s:
+        if c == prev and c == '-':
+            continue
+        else:
+            new_s += c
+            prev = c
+    return new_s
+
 args = sys.argv[1:]
-query = 'XIAOMI 11T/11T PRO (8/12GB+128GB/256GB) ORIGINAL XIAOMI MALAYSIA'
+query = 'iPad 10.2-inch 9th Gen Wi-Fi + Cellular (2021)'
 if len(args) == 1:
 	rows = []
 	
 	with open (args[0], 'r') as file :
 		csvreader = csv.reader(file)
 		header = next(csvreader)
+
 		for row in csvreader:
 			rows.append(row)
+
 		column = []
-		cooolumn = []
 		for row in rows:
 			res = row[1]
 			splitres = res.split(",")
-			cooolumn.append(splitres)
-			column.append(res)
+			column.append(splitres)
+
 		items = []
-		for item in cooolumn:
+		for item in column:
 			items.append(item[0])
+
 		splitquery = query.split(" ")
 		i = 0
 		for q in splitquery:
+
 			highest = 0
-			for item in items:
-				# print(item.lower())
-				# print(query.lower())
-				score = fuzz.partial_ratio(item.lower().replace(" ", ""), q.lower().replace(" ", ""))
+			for row in rows:
+				res = row[1]
+				splitres = res.split(",")
+				score = fuzz.partial_ratio(splitres[0].lower().replace(" ", ""), q.lower().replace(" ", ""))
 				if (score > highest):
 					highest = score
-					# highesti = item
-			# print(highesti)
+
 			higharray = []
-			for item in items:
-				score = fuzz.partial_ratio(item.lower().replace(" ", ""), q.lower().replace(" ", ""))
+			for row in rows:
+				res = row[1]
+				splitres = res.split(",")
+				score = fuzz.partial_ratio(splitres[0].lower().replace(" ", ""), q.lower().replace(" ", ""))
 				if (score > highest - 20):
-					higharray.append(item)
-			items = higharray
-		for item in items:
-			# score = fuzz.partial_ratio(item.lower().replace(" ", ""), query.lower().replace(" ", ""))
-			# if (score > highest):
-			# 	highest = score
-			# 	highesti = item
-		# print(highesti)
+					higharray.append(row)
+
+			rows = higharray
+
+		for item in rows:
 			print(item)
+
+		str = 'HUAWEI MateBook D15 (2021), 15.6"",  i5-1135G7, 8GB/512GB (Intel Iris XE)'
+		str2 = 'huawei-matebook-d15-2021-156-i5-1135g7-8gb512gb-intel-iris-xe'
+		str = str.replace(' ', "zzz").replace('-', "zzz")
+		str = re.sub('[\W\ ]+', '', str)
+		str = str.replace('zzz', "-").lower()
+		str = remove_consec_dash(str)
+		# print(str)
+		# print(str2)
